500c14c122c317af74d0b481df55ea9478fff701
Casey Primozic <cprimozic@stackline.com>
2019-09-24 11:57:25 -0700

    Add some utility types + functions
     * Update build script to copy everything into directory root after building
     * Update .gitignore to ignore all copied files

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
 export type ValueOf<T> = T[keyof T];
 
 export type Without<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+export type ArrayElementOf<T> = T extends (infer E)[] ? E : never;
--- src/util/index.ts
+++ src/util/index.ts
@@ -2,10 +2,7 @@ import * as reactModule from './react';
 export * from './react';
 export const react = reactModule;
 
-export const mapObj = <T, R>(
-  obj: { [key: string]: T },
-  pred: (x: T) => R
-): { [key: string]: R } => {
+export const mapObj = <T, R>(obj: { [key: string]: T }, pred: (x: T) => R): { [key: string]: R } => {
   const mappedObj: { [key: string]: R } = {};
   Object.entries(obj).forEach(([key, val]) => {
     mappedObj[key] = pred(val);
@@ -22,3 +19,13 @@ export const mapObjToMap = <T, R>(obj: { [key: string]: T }, pred: (x: T) => R):
 
   return mapped;
 };
+
+export const isNil = (x: any) => x === null || x === undefined;
+
+export const filterNils = <T>(arr: (T | null | undefined)[]): T[] => arr.filter(x => !isNil(x)) as T[];
+
+export class UnimplementedError extends Error {
+  constructor(message?: string) {
+    super(message || 'Not yet implemented');
+  }
+}
