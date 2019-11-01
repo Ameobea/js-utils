566e4752ddffdfab34e8e68abaeee977fbfa4760
Casey Primozic <cprimozic@stackline.com>
2019-11-01 16:12:48 -0700

    Add `PromiseResolveType` helper type

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,8 @@ export type ValueOf<T> = T[keyof T];
 export type Without<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
 export type ArrayElementOf<T> = T extends (infer E)[] ? E : never;
+
+/**
+ * Returns the type that a given `Promise` resolves to
+ */
+export type PromiseResolveType<P> = P extends Promise<infer T> ? T : never;
